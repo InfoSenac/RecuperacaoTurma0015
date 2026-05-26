ed7669743f72fdf8efc0018776a27c3f7ecb85a6
Samuel <samuelsenac.74@gmail.com>
2026-05-25 21:29:50 -0300

    Edição do Exercicio 01

---
+++ Aula01/Exercícios/SamuelRibeiro/Exercicio01.py
@@ -0,0 +1,6 @@
+def media(n1, n2):
+  media = (n1 + n2) /2
+  return media 
+n1 = float(input("Digite a primeira nota:"))
+n2 = float(input("digite a segunda nota:"))
+print( "a media é esse valor aqui:",media(n1,n2))
